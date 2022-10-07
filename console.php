e9f9c15bd0663b91d6372d8125f602df8d6821d9
Nuša <nusa.jazbinsek@gmail.com>
2022-10-07 21:27:02 +0200

    Naloga 5

--- console.php
+++ console.php
@@ -1,26 +1,59 @@
 <?php
 
+
 if (isset($argv[1]) && is_string($argv[1]) && ctype_alpha($argv[1]) && strlen($argv[1]) > 1 && strlen($argv[1] < 100)) {
 
     $json = file_get_contents('https://api.thedogapi.com/v1/breeds/search?q=' . $argv[1]);
+
+    if ($json === false) {
+        echo "Error!\n";
+        die;
+    }
+
+    if (empty($json)) {
+        echo "No results.\n";
+    }
+
     $array = json_decode($json, TRUE);
 
-    foreach ($array as $item) {
-        echo $item["name"] . "\n";
+    if ($array === null) {
+        echo "Incorrect data!\n"; 
+        die;
     }
+
+    listNames($array); 
 
      
 } else {
 
-    $json = file_get_contents('https://api.thedogapi.com/v1/breeds');
+    $json = @file_get_contents('https://api.thedogapi.com/v1/breeds');
+
+    if ($json === false) {
+        echo "Error!\n";
+        die;
+    }
 
     $array = json_decode($json, TRUE);
 
-    foreach ($array as $item) {
-    echo $item["name"] . "\n";
+
+    if ($array === null) {
+        echo "Incorrect data!\n"; 
+        die;
     }
+
+    listNames($array); 
 
 }
 
+
+
+
+function listNames($array) {
+
+    foreach ($array as $item) {
+        echo $item["name"] . "\n";
+        }
+
+}
 
 ?>
