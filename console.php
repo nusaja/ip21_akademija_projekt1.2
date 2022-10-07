3855fd9eda26642267292dedb998c09a3f83f734
Nuša <nusa.jazbinsek@gmail.com>
2022-10-07 16:52:06 +0200

    Naloga 4

--- console.php
+++ console.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($argv[1])) {
+if (isset($argv[1]) && is_string($argv[1]) && ctype_alpha($argv[1]) && strlen($argv[1]) > 1 && strlen($argv[1] < 100)) {
 
     $json = file_get_contents('https://api.thedogapi.com/v1/breeds/search?q=' . $argv[1]);
     $array = json_decode($json, TRUE);
