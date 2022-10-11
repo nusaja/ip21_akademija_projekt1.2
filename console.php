dd33cb8d046b65ce40937d103e99f9ebecac2c26
Nuša <nusa.jazbinsek@gmail.com>
2022-10-11 18:30:58 +0200

    Naloga 6

--- console.php
+++ console.php
@@ -1,52 +1,51 @@
 <?php
 
 
-if (isset($argv[1]) && is_string($argv[1]) && ctype_alpha($argv[1]) && strlen($argv[1]) > 1 && strlen($argv[1] < 100)) {
+if (isset($argv[1])) {
 
-    $json = file_get_contents('https://api.thedogapi.com/v1/breeds/search?q=' . $argv[1]);
+    if ($argv[1] === "list") {
 
-    if ($json === false) {
-        echo "Error!\n";
-        die;
+        $json = @file_get_contents('https://api.thedogapi.com/v1/breeds');
+
+        $array = decode($json);
+    
+        listNames($array); 
+    
+    } elseif ($argv[1] === "search" && isset($argv[2]) && is_string($argv[2]) && ctype_alpha($argv[2]) && strlen($argv[2]) > 1 && strlen($argv[2] < 100)) {
+
+        $json = file_get_contents('https://api.thedogapi.com/v1/breeds/search?q=' . $argv[2]);
+
+        $array = decode($json);
+    
+        listNames($array); 
+
     }
 
-    if (empty($json)) {
-        echo "No results.\n";
-    }
-
-    $array = json_decode($json, TRUE);
-
-    if ($array === null) {
-        echo "Incorrect data!\n"; 
-        die;
-    }
-
-    listNames($array); 
-
-     
 } else {
-
-    $json = @file_get_contents('https://api.thedogapi.com/v1/breeds');
-
-    if ($json === false) {
-        echo "Error!\n";
-        die;
-    }
-
-    $array = json_decode($json, TRUE);
-
-
-    if ($array === null) {
-        echo "Incorrect data!\n"; 
-        die;
-    }
-
-    listNames($array); 
+    
+    echo "Type in list or search!\n";
 
 }
 
 
+function decode($json) {
 
+    if ($json === false) {
+        echo "Error!\n";
+        die;
+    }
+
+    $array = json_decode($json, TRUE);
+
+
+    if ($array === null) {
+        echo "Incorrect data!\n"; 
+        die;
+    }
+
+    return $array;
+
+}
 
 function listNames($array) {
 
