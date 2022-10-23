6a04005a11e3834ad92ee71e0d0c88c0f96f137e
Nuša <nusa.jazbinsek@gmail.com>
2022-10-23 20:28:26 +0200

    Naloga 8

--- console.php
+++ console.php
@@ -4,17 +4,15 @@ $action = $argv[1] ?? null;
 $breed = $argv[2] ?? null;
 $search = $argv[3] ?? null;
 
-$url = "https://api.the" . $breed . "api.com/v1/breeds";
-
 
 switch ($action) {
     case "list":
-        $array = callApi($url);
+        $array = getData($breed, "");
         listNames($array);
         break;
     case "search":
         if (isset($search) && is_string($search) && ctype_alpha($search) && strlen($search) > 1 && strlen($search < 100)) {
-            $array = callApi($url . '/search?q=' . $search);
+            $array = getData($breed, "/search?q=" . $search);
             listNames($array); 
         } else {
             echo "Invalid search!\n";
@@ -26,11 +24,31 @@ switch ($action) {
 }
 
 
-function callApi($url) {
+function getData($breed, $query) {
+    
+    if ($breed === "both") {
+
+        return array_merge(
+            callApi("dog", $query), 
+            callApi("cat", $query)
+        );
+    }
+
+    return callApi($breed, $query);
+
+}
+
+function callApi($breed, $query) {
+
+    $url = "https://api.the" . $breed . "api.com/v1/breeds" . $query;
 
     $json = @file_get_contents($url); 
     
     $array = decode($json);
+
+    foreach ($array as $key => $value) {
+        $array[$key]["type"] = $breed; 
+    }
 
     return $array;
 
@@ -58,8 +76,24 @@ function decode($json) {
 
 function listNames($array) {
 
-    foreach ($array as $item) {
-        echo $item["name"] . "\n";
+    $newArray = [];
+
+    foreach ($array as $value) {
+        $newArray[$value["name"]] = $value;
+    }
+
+    ksort($newArray);
+
+    foreach ($newArray as $value) {
+        if ($value["type"] === "dog") {
+            $type = "(d) ";
+        } else {
+            $type = "(c) ";
+        }
+    }
+
+    foreach ($newArray as $item) {
+        echo $type . $item["name"] . "\n";
         }
 
 }
